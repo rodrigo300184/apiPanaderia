1334fde07d2be21ebb76bf8296f1a927cd11f014
Rodrigo <rodrigo300184@gmail.com>
2024-03-25 12:36:12 +0100

    Connection and configuration to access db

---
+++ utils/api_connection.js
@@ -0,0 +1,54 @@
+const mysql = require('mysql2/promise');
+import 'dotenv/config';
+
+const userSQL = process.env.USER_SQL;
+const databaseSQL = process.env.DATABASE_SQL;
+const passwordSQL = process.env.PASSWORD_SQL;
+const port = process.env.PORT;
+
+
+export const createDatabaseIfNotExists = async () => {
+  try {
+    // Conectarse al servidor MySQL sin seleccionar una base de datos.
+    const connection = await mysql.createConnection({
+      port: port,
+      host: '127.0.0.1',
+      user: userSQL,
+      password: passwordSQL,
+    });
+
+    // Ejecutar consulta para crear la base de datos si no existe.
+    await connection.query(`CREATE DATABASE IF NOT EXISTS ${databaseSQL}`);
+
+    // Cerrar la conexión sin seleccionar una base de datos.
+    await connection.end();
+
+    
+  } catch (error) {
+    console.error('Error al crear o seleccionar la base de datos:', error);
+    throw error;
+  }
+};
+
+// Llamar a la función createDatabaseIfNotExists al iniciar la aplicación
+createDatabaseIfNotExists();
+
+const pool = mysql.createPool({
+  port: 3307,
+  host: '127.0.0.1',
+  user: userSQL,
+  database: databaseSQL,
+  password: passwordSQL,
+  waitForConnections: true,
+  connectionLimit: 10,
+  maxIdle: 10,
+  idleTimeout: 60000,
+  queueLimit: 0,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 0
+});
+
+export const selectQuery = async (query, values = []) => {
+  const [result] = await pool.execute(query, values);
+  return result;
+};
